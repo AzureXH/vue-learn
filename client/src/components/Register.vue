7130c56b0ed28c249a810b28e4fe20190216d3ce
unknown <songyan@mba.com>
2020-12-28 20:02:29 +0800

    register function, but has bugs in vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="email" v-model="email" />
-    <br />
-    <input
-      type="password"
-      name="password"
-      placeholder="password"
-      v-model="password"
-    />
-    <br />
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title> Register </v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            v-model="email"
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            v-model="password"
+          />
+          <br />
+          <div class="error" v-html="error" />
+          <br />
+          <v-btn class="cyan" @click="register">Register</v-btn >
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -21,6 +36,7 @@ export default {
     return {
       email: "",
       password: "",
+      error: null,
     };
   },
   watch: {
@@ -30,19 +46,21 @@ export default {
   },
   methods: {
     async register() {
-      await AuthenticationService.register({
-        email:this.email,
-        password:this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password,
+        });
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
     },
-  },
-  mounted() {
-    setTimeout(() => {
-      this.email = "hello world";
-    }, 2000);
-  },
+  }
 };
 </script>
 
 <style scoped>
+.error {
+  color: red;
+}
 </style>
--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -1,8 +1,8 @@
 import Api from '@/services/Api'
 
 export default {
-    register(credentials) {
-        console.log(credentials);
-        return Api().post('register', credentials)
-    }
+  register (credentials) {
+    console.log(credentials)
+    return Api().post('register', credentials)
+  }
 }
